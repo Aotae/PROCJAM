effb6d6e8dc78e7b471b19bbe50519381353ca0f
Aotae <nathan.ao.pang@gmail.com>
2024-10-27 14:05:38 -0700

    Some Assets, Can instantiate Room scene in main scene

---
+++ procjam/DungeonGen.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class DungeonGen : Node
+{
+	[Export]
+	public int Room_X{get;set;} = 100;
+	[Export]
+	public int Room_Y {get;set;} = 100;
+	public override void _Ready()
+	{
+		var player = GD.Load<PackedScene>("res://Player.tscn");
+		var scene = GD.Load<PackedScene>("res://Room.tscn");
+		var playerinstance = player.Instantiate();
+		var room = scene.Instantiate();
+		AddChild(room);
+		AddChild(playerinstance);
+		
+	}
+}
