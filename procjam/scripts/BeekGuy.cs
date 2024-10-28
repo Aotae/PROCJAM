895bfffbee5fab8996d205decbc84ef6d942ed5f
KentuckyDriedChicken <dirtdisturbed@gmail.com>
2024-10-27 14:29:00 -1000

    Sorted files into appropriate folders
    Removed Player.tscn

--- procjam/scripts/BeekGuy.cs
+++ procjam/scripts/BeekGuy.cs
@@ -4,7 +4,7 @@ using System;
 public partial class BeekGuy : CharacterBody2D
 {
 	[Export]
-	public int Speed {get; set;} = 400;
+	public int Speed {get; set;} = 100;
 	public Vector2 ScreenSize;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
