81f7d51ba832a85c2c2bbf26ba3edb78df37d143
Aotae <nathan.ao.pang@gmail.com>
2024-10-27 23:26:24 -0700

    Proc Gen

--- procjam/scripts/BeekGuy.cs
+++ procjam/scripts/BeekGuy.cs
@@ -6,6 +6,13 @@ public partial class BeekGuy : CharacterBody2D
 	[Export]
 	public int Speed {get; set;} = 100;
 	public Vector2 ScreenSize;
+
+	public void GetInput()
+    {
+        Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		// GD.Print(inputDirection.IsNormalized());
+        Velocity = inputDirection * Speed;
+    }
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,36 +22,19 @@ public partial class BeekGuy : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
-
-		if (Input.IsActionPressed("move_right"))
-		{
-			velocity.X += 1;
-		}
-
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-		}
-
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
-
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
+		// var Velocity = Vector2.Zero; // The player's movement vector.
 		
-		Position += velocity * Speed * (float)delta;
-		Position = new Vector2(x: Mathf.Clamp(Position.X, -2880, 2880),y: Mathf.Clamp(Position.Y, -1620, 1620));
+		// Position += Velocity * Speed * (float)delta;
+		// Position = new Vector2(x: Mathf.Clamp(Position.X, -2880, 2880),y: Mathf.Clamp(Position.Y, -1620, 1620));
+
+		GetInput();
+		MoveAndSlide();
 		
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("BeekGuySprite");
 
-		if (velocity.Length() > 0)
+		if (Velocity.Length() > 0)
 		{
-			velocity = velocity.Normalized() * Speed;
+			Velocity = Velocity.Normalized() * Speed;
 			animatedSprite2D.Play();
 		}
 		else
@@ -52,35 +42,35 @@ public partial class BeekGuy : CharacterBody2D
 			animatedSprite2D.Stop();
 		}
 		// sprite selection
-		if (velocity.X > 0 && velocity.Y > 0)
+		if (Velocity.X > 0 && Velocity.Y > 0)
 		{
 			animatedSprite2D.Animation = "FrontRight";
 		}
-		else if (velocity.X > 0 && velocity.Y < 0)
+		else if (Velocity.X > 0 && Velocity.Y < 0)
 		{
 			animatedSprite2D.Animation = "BackRight";
 		}
-		else if (velocity.X < 0 && velocity.Y > 0)
+		else if (Velocity.X < 0 && Velocity.Y > 0)
 		{
 			animatedSprite2D.Animation = "FrontLeft";
 		}
-		else if (velocity.X < 0 && velocity.Y < 0)
+		else if (Velocity.X < 0 && Velocity.Y < 0)
 		{
 			animatedSprite2D.Animation = "BackLeft";
 		}
-		else if (velocity.X > 0)
+		else if (Velocity.X > 0)
 		{
 			animatedSprite2D.Animation = "Right";
 		}
-		else if (velocity.X < 0)
+		else if (Velocity.X < 0)
 		{
 			animatedSprite2D.Animation = "Left";
 		}
-		else if (velocity.Y > 0)
+		else if (Velocity.Y > 0)
 		{
 			animatedSprite2D.Animation = "Front";
 		}
-		else if (velocity.Y < 0)
+		else if (Velocity.Y < 0)
 		{
 			animatedSprite2D.Animation = "Back";
 		}
